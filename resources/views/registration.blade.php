9b468fded32c03c8939514fa477d7b8c84332d52
root <phoenixD112>
2015-10-25 04:07:21 +0000

    Changed registration page.

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -1,8 +1,10 @@
 @include('includes.header') 
       <div class="charities" id="charities" style="background-color: #fff;">
         <div style="height: 400px; padding-top: 60px; margin-left: 25px; margin-right: 25px;">
-            <h2>Privacy</h2>
-
+            <h2>Complete Registration</h2>
+			<form method="/secure/user">
+				<button class="btn btn-primary btn-lg">Register</button>	
+			</form>
         </div>
     </div> 
 @include('includes.footer') 
